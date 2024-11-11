e7015b641cab19e5761e691fe0617492cd543dc0
lotfi20 <66782542+lotfi20@users.noreply.github.com>
2024-11-11 03:34:02 +0100

    fiixing the frontt

--- etudiant-angular/src/app/Core/Services/student.service.ts
+++ etudiant-angular/src/app/Core/Services/student.service.ts
@@ -12,36 +12,31 @@ export class StudentService {
   constructor(private http: HttpClient) { }
 
   API = 'http://192.168.33.0:8082/kaddem/etudiant';
-
   public registerStudent(studentData: any) {
     return this.http.post<student>(this.API + '/add-etudiant', studentData);
   }
-
   public getStudents() {
     return this.http.get(this.API + '/retrieve-all-etudiants');
   }
-
   public deleteStudent(idEtudiant: any) {
     return this.http.delete(this.API + '/remove-etudiant/'+ idEtudiant);
   }
-
   public updateStudents(student: any) {
     return this.http.put(this.API + '/update-etudiant', student);
   }
-  
+
   public assignStudent(idEtudiant:any,idDepart:any){
 
         return this.http.post<number>(`${this.API}/assignetudianttodepartment/${idEtudiant}/${idDepart}`,{});
-
   }
 
    public getDepartements(){
 
-        return this.http.get<departement[]>('http://192.168.1.0:8082/kaddem/DepartmentController/displayalldepartment' );
+        return this.http.get<departement[]>('http://192.168.33.0:8082/kaddem/DepartmentController/displayalldepartment' );
   }
 
  public getallequipe() {
-    return this.http.get<equipe[]>('http://192.168.1.0:8082/kaddem/EquipeController/displayEquipes' );
+    return this.http.get<equipe[]>('http://192.168.33.0:8082/kaddem/EquipeController/displayEquipes' );
   }
   
   public assignstudenttoequipe(idEtudiant:any,ideq:any){
